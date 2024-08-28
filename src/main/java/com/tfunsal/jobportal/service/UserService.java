1f47f7ebab2b9bc0be57bf3aecdec270cd6aee81
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2024-08-28 13:35:04 +0300

    feat: Updated  to handle  and  creation based on user type.

--- src/main/java/com/tfunsal/jobportal/service/UserService.java
+++ src/main/java/com/tfunsal/jobportal/service/UserService.java
@@ -1,6 +1,10 @@
 package com.tfunsal.jobportal.service;
 
+import com.tfunsal.jobportal.entity.JobSeeker;
+import com.tfunsal.jobportal.entity.Recruiter;
 import com.tfunsal.jobportal.entity.User;
+import com.tfunsal.jobportal.repository.JobSeekerRepository;
+import com.tfunsal.jobportal.repository.RecruiterRepository;
 import com.tfunsal.jobportal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,16 +16,28 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final JobSeekerRepository jobSeekerRepository;
+    private final RecruiterRepository recruiterRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, JobSeekerRepository jobSeekerRepository, RecruiterRepository recruiterRepository) {
         this.userRepository = userRepository;
+        this.jobSeekerRepository = jobSeekerRepository;
+        this.recruiterRepository = recruiterRepository;
     }
 
     public User create(User user){
         user.setActive(true);
         user.setRegistrationDate(new Date(System.currentTimeMillis()));
-        return userRepository.save(user);
+        User savedUser = userRepository.save(user);
+
+        Long userTypeId = user.getUserTypeId().getId();
+        if (userTypeId == 1){
+            recruiterRepository.save(new Recruiter(savedUser));
+        }else {
+            jobSeekerRepository.save(new JobSeeker(savedUser));
+        }
+        return savedUser;
     }
 
     public Optional<User> getUserByEmail(String email){
